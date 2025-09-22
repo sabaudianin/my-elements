d9459e82b7f6e45fea711f4f43c84d73f317512c
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 <sabaudianin@gmail.com>
2025-09-22 17:22:10 +0200

    Card animation perspective

--- src/components/Card/FlipCard/FlipCard.tsx
+++ src/components/Card/FlipCard/FlipCard.tsx
@@ -1,15 +1,15 @@
 export const FlipCard = () => {
   return (
-    <section className="flex h-screen items-center justify-center bg-blue-100">
-      <div className="transition-100 h-2/3 w-1/4 cursor-pointer rounded-md border-1 border-solid font-semibold text-black shadow-[9px_18px_9px_0px_rgba(0,_0,_0,_0.35)]">
-        <div className="p-4">
-          <div className="">
+    <section className="flex h-screen items-center justify-center bg-blue-200 [perspective:300px]">
+      <div className="realtive h-2/3 w-1/4 transform-gpu cursor-pointer rounded-md border-1 border-solid font-semibold text-black shadow-[9px_18px_9px_0px_rgba(0,_0,_0,_0.35)] transition-transform duration-700 [transform-style:preserve-3d] hover:[transform:rotateY(180deg)]">
+        <div className="absolute h-full w-full bg-[url('/avatarMini.jpg')] bg-cover bg-center p-4 text-white [backface-visibility:hidden]">
+          <div className="absolute bottom-10 left-[25%] text-center">
             <h2 className="py-2 text-2xl">Rafal Bobko</h2>
             <p className="">1k followers on Github</p>
           </div>
         </div>
-        <div className="p-4">
-          <div>
+        <div className="absolute p-4 [backface-visibility:hidden]">
+          <div className="">
             <h3 className="py-2 text-xl">Rafal Bobko</h3>
             <p className="">
               Rafal Bobko is a high class modern Developer. Graduated many courser, bootcamps and
