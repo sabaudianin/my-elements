ed42b068b29c67c2a1e8ddf94c3b1202937d305d
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 <sabaudianin@gmail.com>
2025-11-23 20:37:36 +0100

    feat spinning coin component

---
+++ src/components/SpinnedBtc/SpinedBtc.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const SpinedBtc = () => {
+  return (
+    <>
+      <style></style>
+
+      <div>
+        SpinedBtc
+        <div className="coin">
+          <div className="side heads h-32 w-32 border">B</div>
+          <div className="side tails h-32 w-32">B </div>
+        </div>
+      </div>
+    </>
+  );
+};
