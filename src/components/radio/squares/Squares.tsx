bd8581bf4a06cfa004d0b0c4aa9063faedbb4efe
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 <sabaudianin@gmail.com>
2026-01-16 18:24:49 +0100

    radio squares in pure css

---
+++ src/components/radio/squares/Squares.tsx
@@ -0,0 +1,23 @@
+<div className="radio-inputs">
+  <label>
+    <input className="radio-input" type="radio" name="engine" />
+    <span className="radio-tile">
+      <span className="radio-icon"></span>
+      <span className="radio-label">Bicycle</span>
+    </span>
+  </label>
+  <label>
+    <input className="radio-input" type="radio" name="engine" />
+    <span className="radio-tile">
+      <span className="radio-icon"></span>
+      <span className="radio-label">Bicycle</span>
+    </span>
+  </label>
+  <label>
+    <input className="radio-input" type="radio" name="engine" />
+    <span className="radio-tile">
+      <span className="radio-icon"></span>
+      <span className="radio-label">Car</span>
+    </span>
+  </label>
+</div>;
